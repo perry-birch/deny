e0e2c92f973aaca1bb18c88bbe162d8266065645
Perry Birch <perrybirch@vizidrix.com>
2013-04-08 01:34:32 -0700

    Bump to fix typo...

--- lib/deny.dart
+++ lib/deny.dart
@@ -8,7 +8,7 @@ import 'src/oauth_credentials.dart';
 import 'src/query_string.dart';
 
 export 'src/authorization_exception.dart';
-export 'src/expireation_exception.dart';
+export 'src/expiration_exception.dart';
 export 'src/instagram.dart';
 export 'src/oauth2.dart';
 export 'src/oauth_credentials.dart';
